e1ca209250a49adf10163644347f2f0cdd4b41cc
andretorresb <andret44@icloud.com>
2025-09-25 15:42:41 -0400

    Terminando a Aula 5

--- Programação_Orientada_Objetos/POO_aula5.py
+++ Programação_Orientada_Objetos/POO_aula5.py
@@ -8,16 +8,32 @@ class Pessoa:
 
 class Funcionario(Pessoa):
     def __init__(self, nome, idade, cargo):
+        super().__init__(nome,idade)
         self.cargo = cargo
 
     def trabalhar(self):
-        print('{}')
+        print(f'{self.nome} esta trabalhando como {self.cargo}!')
 
 class Cliente(Pessoa):
-    pass
+    def __init__(self, nome, idade, saldo):
+        super().__init__(nome,idade)
+        self.saldo = saldo
+
+    def comprar(self, valor_compra):
+        if valor_compra <= self.saldo:
+            self.saldo -= valor_compra
+            print(f'A sua compra de {valor_compra} reais foi Aprovada! Seu saldo atual é de: R${self.saldo}')
+        else:
+            print(f'Saldo insuficiente')
+
+
 
 pessoa1 = Pessoa('Andre', 13)
-pessoa1.apresentar()
+#pessoa1.apresentar()
 
-cliente1 = Cliente('Yasmin', 16)
+f1 = Funcionario('Maria', 38, 'Analista de suporte')
+#f1.trabalhar()
 
+cliente1 = Cliente('Yasmin', 16, 200)
+#cliente1.apresentar()
+cliente1.comprar(300)
